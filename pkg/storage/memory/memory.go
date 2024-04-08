c0ef3e0e8019c4aab31bb6bbfbbfabd30b57c2d8
EvansChang <85141291@qq.com>
2024-04-08 19:54:35 +0800

    init memory storage

--- pkg/storage/memory/memory.go
+++ pkg/storage/memory/memory.go
@@ -1,4 +1,53 @@
 package memory
 
-type MemoryStorage struct {
+import (
+	"github.com/mcache-team/mcache/pkg/apis/v1/item"
+	"github.com/mcache-team/mcache/pkg/apis/v1/storage"
+)
+
+type Memory struct {
+	prefixList []string
+	dataMap    map[string][]*item.Item
+}
+
+var _ storage.Storage = &Memory{}
+
+func (m *Memory) GetOne(prefix string) (interface{}, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *Memory) ListPrefixData(prefix string) ([]interface{}, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *Memory) CountPrefixData(prefix string) (int, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *Memory) ListPrefix(prefixStr string) ([]string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *Memory) CountPrefix(prefixStr string) (int, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *Memory) Insert(prefix string, data interface{}, opt ...item.Option) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *Memory) Update(prefix string, data interface{}, opt ...item.Option) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m *Memory) Delete(prefix string) error {
+	//TODO implement me
+	panic("implement me")
 }
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,1 +1,48 @@
 package storage
+
+import (
+	"github.com/mcache-team/mcache/pkg/apis/v1/item"
+	"github.com/mcache-team/mcache/pkg/apis/v1/storage"
+	"github.com/mcache-team/mcache/pkg/storage/memory"
+)
+
+type Storage struct {
+	store     storage.Storage
+	storeType string
+}
+
+func NewStorage() storage.Storage {
+	return &Storage{store: &memory.Memory{}, storeType: "memory"}
+}
+
+func (s *Storage) GetOne(prefix string) (interface{}, error) {
+	return s.store.GetOne(prefix)
+}
+
+func (s *Storage) ListPrefixData(prefix string) ([]interface{}, error) {
+	return s.store.ListPrefixData(prefix)
+}
+
+func (s *Storage) CountPrefixData(prefix string) (int, error) {
+	return s.store.CountPrefixData(prefix)
+}
+
+func (s *Storage) ListPrefix(prefixStr string) ([]string, error) {
+	return s.store.ListPrefix(prefixStr)
+}
+
+func (s *Storage) CountPrefix(prefixStr string) (int, error) {
+	return s.store.CountPrefix(prefixStr)
+}
+
+func (s *Storage) Insert(prefix string, data interface{}, opt ...item.Option) error {
+	return s.store.Insert(prefix, data, opt...)
+}
+
+func (s *Storage) Update(prefix string, data interface{}, opt ...item.Option) error {
+	return s.store.Update(prefix, data, opt...)
+}
+
+func (s *Storage) Delete(prefix string) error {
+	return s.store.Delete(prefix)
+}
