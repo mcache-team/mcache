75fdc24e932c18005f02c728dcab60fa1368e6e8
EvansChang <85141291@qq.com>
2024-04-30 17:04:01 +0800

    add prefix router

---
+++ pkg/services/normal/prefix.go
@@ -0,0 +1,41 @@
+package normal
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mcache-team/mcache/pkg/services/response"
+	"github.com/mcache-team/mcache/pkg/storage"
+)
+
+func init() {
+	h := &PrefixHandler{}
+	controller[h.BasePath()] = h
+}
+
+var _ Controller = &PrefixHandler{}
+
+type PrefixHandler struct {
+}
+
+func (d *PrefixHandler) BasePath() string {
+	return "/v1/prefix"
+}
+
+func (d *PrefixHandler) RegisterRouter(r *gin.Engine) {
+	group := r.Group(d.BasePath())
+	group.GET("", d.list)
+	group.GET("/count", d.count)
+}
+
+func (d *PrefixHandler) list(ctx *gin.Context) {
+	data, err := storage.StorageClient.ListPrefix("")
+	if err != nil {
+		response.ResponseNotFound(ctx)
+		return
+	}
+	response.ResponseData(ctx, data)
+}
+
+func (d *PrefixHandler) count(ctx *gin.Context) {
+	count := storage.StorageClient.CountPrefix("")
+	response.ResponseData(ctx, gin.H{"count": count})
+}
